a74199d85e0ebe0cac83b2977c05bc2211cd4040
Zach Dean <zdean3721@gmail.com>
2019-11-03 21:37:02 -0600

    updated page formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { ChatExampleData } from './data/chat-example-data';
+
 import { UsersService } from './user/users.service';
 import { ThreadsService } from './thread/threads.service';
 import { MessagesService } from './message/messages.service';
@@ -10,9 +11,9 @@ import { MessagesService } from './message/messages.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ChatApp';
-
-  constructor(public messagesService: MessagesService, public threadsService: ThreadsService, public usersService: UsersService) {
+    constructor(public messagesService: MessagesService,
+              public threadsService: ThreadsService,
+              public usersService: UsersService) {
     ChatExampleData.init(messagesService, threadsService, usersService);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
+import { UsersService } from './user/users.service';
+import { ThreadsService } from './thread/threads.service';
+import { MessagesService } from './message/messages.service';
+
 import { AppComponent } from './app.component';
-import { ChatThreadsComponent } from './chat-threads/chat-threads.component';
-import { ChatThreadComponent } from './chat-thread/chat-thread.component';
-import { ChatWindowComponent } from './chat-window/chat-window.component';
 import { ChatMessageComponent } from './chat-message/chat-message.component';
+import { ChatThreadComponent } from './chat-thread/chat-thread.component';
 import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
+import { ChatThreadsComponent } from './chat-threads/chat-threads.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
+import { ChatPageComponent } from './chat-page/chat-page.component';
+import { FromNowPipe } from './pipes/from-now.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatThreadsComponent,
-    ChatThreadComponent,
-    ChatWindowComponent,
     ChatMessageComponent,
-    ChatNavBarComponent
+    ChatThreadComponent,
+    ChatNavBarComponent,
+    ChatThreadsComponent,
+    ChatWindowComponent,
+    ChatPageComponent,
+    FromNowPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    MessagesService, ThreadsService, UsersService
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
